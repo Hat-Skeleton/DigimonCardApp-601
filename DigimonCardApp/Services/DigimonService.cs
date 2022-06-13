604b692481c6ed5396ab4d4c2968cb112615b75c
Hat-Skeleton <codydpc@hotmail.com>
2022-06-13 22:10:48 +1200

    Added Styles

---
+++ DigimonCardApp/Services/DigimonService.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http.Json;
+
+namespace DigimonCardApp;
+
+public class DigimonService
+{
+    HttpClient httpClient;
+    public DigimonService()
+    {
+        httpClient = new HttpClient();
+    }
+
+    List<DigimonCard> digimonCardList = new();
+    public async Task<List<DigimonCard>> GetDigimonCard()
+    {
+        //    if (digimonCardList?.Count > 0)
+        //        return digimonCardList;
+
+        //FOR ONLINE USE
+
+        //var url = "https://raw.githubusercontent.com/Hat-Skeleton/DigimonProjectJson/main/DigimonApp.json?token=GHSAT0AAAAAABU4UXQGTG54INKMTWKLM4TOYUMZBBA";
+
+        //var response = await httpClient.GetAsync(url);
+
+        //if (response.IsSuccessStatusCode)
+        //{
+        //    digimonCardList = await response.Content.ReadFromJsonAsync<List<DigimonCard>>();
+        //}
+
+        using var stream = await FileSystem.OpenAppPackageFileAsync("DigimonApp.json");
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        digimonCardList = JsonSerializer.Deserialize<List<DigimonCard>>(contents);
+
+        return digimonCardList;
+    }
+}
