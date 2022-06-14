cce55c277cb81b7aef65688ebc424025cf434e5b
Hat-Skeleton <codydpc@hotmail.com>
2022-06-14 20:53:54 +1200

    DigimonCard model added

--- DigimonCardApp/Model/DigimonCard.cs
+++ DigimonCardApp/Model/DigimonCard.cs
@@ -2,4 +2,23 @@
 
 public class DigimonCard
 {
+    public string CardID { get; set; }
+    public string CardColour1 { get; set; }
+    public string CardColour2 { get; set; }
+    public string SetNumber { get; set; }
+    public string Rarity { get; set; }
+    public string CardType { get; set; }
+    public string Level { get; set; }
+    public string Name { get; set; }
+    public string Form { get; set; }
+    public string Attribute { get; set; }
+    public string Type { get; set; }
+    public string DP { get; set; }
+    public string PlayCost { get; set; }
+    public string DigivolveCost1 { get; set; }
+    public string DigivolveCost2 { get; set; }
+    public string Effect { get; set; }
+    public string DigivolveEffect { get; set; }
+    public string SecurityEffect { get; set; }
+    public string Set { get; set; }
 }
