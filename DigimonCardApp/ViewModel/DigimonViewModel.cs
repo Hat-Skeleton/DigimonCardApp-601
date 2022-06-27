d3c70bd4a950371d53a27ed073074aec90c86ad0
Hat-Skeleton <codydpc@hotmail.com>
2022-06-27 13:34:48 +1200

    Navigation works! :) also more sets up

--- DigimonCardApp/ViewModel/DigimonViewModel.cs
+++ DigimonCardApp/ViewModel/DigimonViewModel.cs
@@ -22,12 +22,51 @@ public partial class DigimonViewModel : BaseViewModel
     async Task Navigate(string setNo)
     {
         Set = setNo;
+        await ClearCards();
         await Shell.Current.GoToAsync($"{nameof(MainPage)}?Set={Set}");
-        await GetDigimonCardsAsync();
+        switch (Set)
+        {
+            case "New Evolution":
+                await GetBT1CardsAsync();
+                break;
 
+            case "Ultimate Power":
+                await GetBT2CardsAsync();
+                break;
+            case "Union Impact":
+                await GetBT3CardsAsync();
+                break;
+            case "Great Legend":
+                await GetBT4CardsAsync();
+                break;
+            case "Battle Of Omni":
+                await GetBT5CardsAsync();
+                break;
+            case "Double Diamond":
+                await GetBT6CardsAsync();
+                break;
+
+        }
     }
 
-    [ObservableProperty]
+    [ICommand]
+    async Task ClearCards()
+    {
+        try
+        {
+            if (DigimonCards.Count != 0)
+                DigimonCards.Clear();
+        }
+        
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Here's why: {ex.Message}", "OK");
+        }
+    }
+
+        [ObservableProperty]
     bool isRefreshing;
 
 
@@ -91,7 +130,7 @@ public partial class DigimonViewModel : BaseViewModel
     }
 
     [ICommand]
-    async Task GetDigimonCardsAsync()
+    async Task GetBT1CardsAsync()
     {
         string set1 = "RELEASE SPECIAL BOOSTER Ver.1.0【BT01-03】";
         string set2 = "RELEASE SPECIAL BOOSTER Ver.1.5【BT01-03】";
@@ -121,7 +160,230 @@ public partial class DigimonViewModel : BaseViewModel
                     }
                 }
             }
-            DigimonCards = new ObservableCollection<DigimonCard>(DigimonCards.OrderByDescending(i => i.CardID).ToList());
+            //DigimonCards = new ObservableCollection<DigimonCard>(DigimonCards.OrderByDescending(i => i.CardID).ToList());
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Here's why: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+            isRefreshing = false;
+        }
+    }
+
+    [ICommand]
+    async Task GetBT2CardsAsync()
+    {
+        string set1 = "RELEASE SPECIAL BOOSTER Ver.1.0【BT01-03】";
+        string set2 = "RELEASE SPECIAL BOOSTER Ver.1.5【BT01-03】";
+        string dash1 = "Dash Pack Ver. 1.0";
+        string dash2 = "Dash Pack Ver. 1.5";
+        if (IsBusy)
+            return;
+        try
+        {
+
+            //try adding to list then order, then add to DigimonCards
+            IsBusy = true;
+            var digimonCards = await digimonService.GetDigimonCard();
+
+            if (DigimonCards.Count != 0)
+                DigimonCards.Clear();
+
+            foreach (var digimoncard in digimonCards)
+            {
+
+                string cardid = digimoncard.CardID;
+                if (cardid.Contains("BT2-"))
+                {
+                    if (digimoncard.Set == set1 || digimoncard.Set == set2 || digimoncard.Set == dash1 || digimoncard.Set == dash2)
+                    {
+                        DigimonCards.Add(digimoncard);
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Here's why: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+            isRefreshing = false;
+        }
+    }
+
+    [ICommand]
+    async Task GetBT3CardsAsync()
+    {
+        string set1 = "RELEASE SPECIAL BOOSTER Ver.1.0【BT01-03】";
+        string set2 = "RELEASE SPECIAL BOOSTER Ver.1.5【BT01-03】";
+        string dash1 = "Dash Pack Ver. 1.0";
+        string dash2 = "Dash Pack Ver. 1.5";
+        if (IsBusy)
+            return;
+        try
+        {
+
+            //try adding to list then order, then add to DigimonCards
+            IsBusy = true;
+            var digimonCards = await digimonService.GetDigimonCard();
+
+            if (DigimonCards.Count != 0)
+                DigimonCards.Clear();
+
+            foreach (var digimoncard in digimonCards)
+            {
+
+                string cardid = digimoncard.CardID;
+                if (cardid.Contains("BT3-"))
+                {
+                    if (digimoncard.Set == set1 || digimoncard.Set == set2 || digimoncard.Set == dash1 || digimoncard.Set == dash2)
+                    {
+                        DigimonCards.Add(digimoncard);
+                    }
+                }
+            }
+           
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Here's why: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+            isRefreshing = false;
+        }
+    }
+
+    [ICommand]
+    async Task GetBT4CardsAsync()
+    {
+        string set1 = "BOOSTER -GREAT LEGEND- [BT04]";
+
+        if (IsBusy)
+            return;
+        try
+        {
+
+            //try adding to list then order, then add to DigimonCards
+            IsBusy = true;
+            var digimonCards = await digimonService.GetDigimonCard();
+
+            if (DigimonCards.Count != 0)
+                DigimonCards.Clear();
+
+            foreach (var digimoncard in digimonCards)
+            {
+
+                string cardid = digimoncard.CardID;
+                if (cardid.Contains("BT4-"))
+                {
+                    if (digimoncard.Set == set1)
+                    {
+                        DigimonCards.Add(digimoncard);
+                    }
+                }
+            }
+            
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Here's why: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+            isRefreshing = false;
+        }
+    }
+
+    [ICommand]
+    async Task GetBT5CardsAsync()
+    {
+        string set1 = "BOOSTER -BATTLE OF OMNI- [BT05]";
+
+        if (IsBusy)
+            return;
+        try
+        {
+
+            //try adding to list then order, then add to DigimonCards
+            IsBusy = true;
+            var digimonCards = await digimonService.GetDigimonCard();
+
+            if (DigimonCards.Count != 0)
+                DigimonCards.Clear();
+
+            foreach (var digimoncard in digimonCards)
+            {
+
+                string cardid = digimoncard.CardID;
+                if (cardid.Contains("BT5-"))
+                {
+                    if (digimoncard.Set == set1)
+                    {
+                        DigimonCards.Add(digimoncard);
+                    }
+                }
+            }
+
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex);
+            await Shell.Current.DisplayAlert("Error!",
+                $"Here's why: {ex.Message}", "OK");
+        }
+        finally
+        {
+            IsBusy = false;
+            isRefreshing = false;
+        }
+    }
+
+    [ICommand]
+    async Task GetBT6CardsAsync()
+    {
+        string set1 = "BOOSTER DOUBLE DIAMOND [BT-06]";
+
+        if (IsBusy)
+            return;
+        try
+        {
+
+            //try adding to list then order, then add to DigimonCards
+            IsBusy = true;
+            var digimonCards = await digimonService.GetDigimonCard();
+
+            if (DigimonCards.Count != 0)
+                DigimonCards.Clear();
+
+            foreach (var digimoncard in digimonCards)
+            {
+
+                string cardid = digimoncard.CardID;
+                if (cardid.Contains("BT6-") || cardid.Contains("BT1-"))
+                {
+                    if (digimoncard.Set == set1)
+                    {
+                        DigimonCards.Add(digimoncard);
+                    }
+                }
+            }
+
         }
         catch (Exception ex)
         {
