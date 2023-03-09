edcd8a9427b6644caefbb2bc9f1cabdf477ceeca
Cody <89228187+Hat-Skeleton@users.noreply.github.com>
2023-03-10 06:32:37 +1300

    Testing

--- DigimonCardApp/Model/DigimonCard.cs
+++ DigimonCardApp/Model/DigimonCard.cs
@@ -2,23 +2,24 @@
 
 public class DigimonCard
 {
-    public string CardID { get; set; }
-    public string CardColour1 { get; set; }
-    public string CardColour2 { get; set; }
-    public string SetNumber { get; set; }
+    public string Card_id { get; set; }
+    public string Card_colour_1 { get; set; }
+    public string Card_colour_2 { get; set; }
+    public string Cardno { get; set; }
     public string Rarity { get; set; }
-    public string CardType { get; set; }
-    public string Level { get; set; }
-    public string Name { get; set; }
+    public string Card_type { get; set; }
+    public string Cardlv { get; set; }
+    public string Card_name { get; set; }
     public string Form { get; set; }
     public string Attribute { get; set; }
     public string Type { get; set; }
-    public string DP { get; set; }
-    public string PlayCost { get; set; }
-    public string DigivolveCost1 { get; set; }
-    public string DigivolveCost2 { get; set; }
+    public int? Dp { get; set; }
+    public int? Play_cost { get; set; }
+    public string Digivolve_cost_1 { get; set; }
+    public string Digivolve_cost_2 { get; set; }
     public string Effect { get; set; }
-    public string DigivolveEffect { get; set; }
-    public string SecurityEffect { get; set; }
-    public string Set { get; set; }
+    public string Digivolve_effect { get; set; }
+    public string Security_effect { get; set; }
+    public string Card_set { get; set; }
+
 }
